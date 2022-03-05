41d00a8bd456dab89bfb99473186955515948aa5
Tiago Lacerda <tblacerda@outlook.com>
2022-03-05 19:05:27 -0300

    commit

--- install_papaya.R
+++ install_papaya.R
@@ -2,3 +2,11 @@ if(!"tinytex" %in% rownames(installed.packages())) install.packages("tinytex")
 
 tinytex::install_tinytex()
 
+# Install devtools package if necessary
+if(!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
+
+# Install the stable development verions from GitHub
+devtools::install_github("crsh/papaja")
+
+# Install the latest development snapshot from GitHub
+devtools::install_github("crsh/papaja@devel")
